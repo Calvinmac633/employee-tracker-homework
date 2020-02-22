d4d4e5197179fb438b4334dd5ecd9a957765428c
Calvin Macintosh <calvinmac633@gmail.com>
2020-02-22 00:35:06 -0500

    need to commit more

--- seeds.sql
+++ seeds.sql
@@ -6,32 +6,32 @@ USE employeeDB;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(15,0)
-  department_id INT(3,0) NOT NULL,
+  salary DECIMAL(15,0),
+  department_id INT(3) NOT NULL,
   PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT(3,0) NOT NULL,
-  manager_id INT(3,0) NOT NULL,
+  role_id INT(3) NOT NULL,
+  manager_id INT(3) NULL,
   PRIMARY KEY (id)
-)
+);
 
 INSERT INTO department (name)
-VALUES ("Accounting", "Finance", "Legal");
+VALUES ("Accounting"), ("Finance"), ("Legal");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant", 60, 1), ("Analyst", 70, 2), ("Lawyer", 80, 3);
+VALUES ("Accountant", 60000, 1), ("Analyst", 70000, 2), ("Legal Associate", 65000, 3), ("Partner", 100000, 1), ("Actuary", 120000, 2), ("Lawyer", 110000, 3) ;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Calvin", "Macintosh", 2, 1);
+VALUES ("Calvin", "Macintosh", 2, 2), ("Bossman", "Jones", 5, null) ;
--- server.js
+++ server.js
@@ -45,6 +45,15 @@ function runSearch() {
                 case "View All Employees":
                     viewEmployees();
                     break;
+                case "View All Employees By Department":
+                    viewEmployeesDept();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
             }
         })
 }
@@ -52,15 +61,15 @@ function runSearch() {
 function viewEmployees() {
     //
     const query =
-        `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, 
-        r.salary, CONCAT(em.first_name, ' ', em.last_name) AS manager
+        `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS dept_name, 
+        r.salary, CONCAT(emp.first_name, ' ', emp.last_name) AS manager
         FROM employee e
         LEFT JOIN role r
 	    ON e.role_id = r.id
         LEFT JOIN department d
         ON d.id = r.department_id
-        LEFT JOIN employee em
-        ON em.id = e.manager_id`
+        LEFT JOIN employee emp
+        ON emp.id = e.manager_id`
 
     connection.query(query, function (err, res) {
         if (err) throw err;
@@ -70,4 +79,115 @@ function viewEmployees() {
 
         runSearch();
     });
+}
+
+function addEmployee() {
+    //use query to selelect role table
+    //store into a roleChoices const
+    //use this to put into addEmployeePrompt
+    //ask first, last, role, manager first and last
+
+    const query = "SELECT id, title FROM role";
+    connection.query(query, function (err, res) {
+        console.log(res)
+    })
+
+    connection.query(`SELECT * FROM role`, function (err, res) {
+        if (err) throw err;
+        console.log(res)
+        inquirer
+            .prompt([
+                {
+                    name: "first_name",
+                    type: "input",
+                    message: "What is the employee's first name?"
+                },
+                {
+                    name: "last_name",
+                    type: "input",
+                    message: "What is the employee's last name?"
+                },
+                {
+                    name: "title",
+                    type: "rawlist",
+                    message: "What is the employee's title?",
+                    choices: function () {
+                        let choiceArray = [];
+                        for (let i = 0; i < res.length; i++) {
+                            choiceArray.push(res[i].title);
+                        }
+                        return choiceArray;
+                    }
+                }
+            ])
+            .then(function (answer) {
+                console.log(answer.title)
+                let title = answer.title;
+                switch (title) {
+                    case "Accountant":
+                        title = 1;
+                        break;
+                    case "Analyst":
+                        title = 2;
+                        break;
+                    case "Legal Associate":
+                        title = 3;
+                        break;
+                    case "Partner":
+                        title = 4;
+                        break;
+                    case "Actuary":
+                        title = 5;
+                        break;
+                    case "Lawyer":
+                        title = 6;
+                        break;
+                }
+                let query = `INSERT INTO employee SET ?`;
+                connection.query(query,
+                    {
+                        first_name: answer.first_name,
+                        last_name: answer.last_name,
+                        role_id: title
+                    },
+                    function (err, res) {
+                        if (err) throw err;
+                        console.table(res);
+                        console.log(res.insertedRows + " Rows inserted")
+                        runSearch();
+                    }
+                )
+            })
+    })
+}
+
+function removeEmployee() {
+    const query = "SELECT id, first_name, last_name FROM employee";
+    connection.query(query, function (err, res) {
+        console.log(res)
+        inquirer
+        .prompt([
+            {
+                name: "remove",
+                type: "rawlist",
+                message: "Select and employee to remove...",
+                choices: function () {
+                    let choiceArray = [];
+                    for (let i = 0; i < res.length; i++) {
+                        choiceArray.push(res[i].first_name);
+                    }
+                    return choiceArray;
+                }
+            }
+        ])
+        .then(function(answer) {
+            console.log(answer.remove)
+            const query = "DELETE FROM employee WHERE ?";
+            connection.query(query, [{first_name: answer.remove }], function(err, res) {
+                if (err) throw err;
+                console.log("EMPLOYEE HAS BEEN DELETED");
+                runSearch();
+            })
+        })
+    })
 }
