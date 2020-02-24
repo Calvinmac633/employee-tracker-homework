a7609e03ae7306ba3a73727b4a08b44ab222431f
Calvin Macintosh <calvinmac633@gmail.com>
2020-02-24 18:49:02 -0500

    updating prompt

--- server.js
+++ server.js
@@ -28,16 +28,16 @@ function runSearch() {
             message: "What would you like to do?",
             choices: [
                 "View All Employees",
-                "View All Employees By Department",
-                "View All Employees By Manager",
+                // "View All Employees By Department",
+                // "View All Employees By Manager",
                 "Add Employee",
                 "Remove Employee",
-                "Update Employee Role",
-                "Update Employee Manager",
-                "View All Roles",
-                "Add Role",
-                "Remove Role",
-                "Exit"
+                // "Update Employee Role",
+                // "Update Employee Manager",
+                // "View All Roles",
+                // "Add Role",
+                // "Remove Role",
+                // "Exit"
             ]
         })
         .then(function (answer) {
@@ -190,5 +190,86 @@ function removeEmployee() {
                 runSearch();
             })
         })
+    })
+}
+
+function addEmployee() {
+    //use query to selelect role table
+    //store into a roleChoices const
+    //use this to put into addEmployeePrompt
+    //ask first, last, role, manager first and last
+
+    const query = "SELECT id, title FROM role";
+    connection.query(query, function (err, res) {
+        console.log(res)
+    })
+
+    connection.query(`SELECT * FROM role`, function (err, res) {
+        if (err) throw err;
+        console.log(res)
+        inquirer
+            .prompt([
+                {
+                    name: "first_name",
+                    type: "input",
+                    message: "What is the employee's first name?"
+                },
+                {
+                    name: "last_name",
+                    type: "input",
+                    message: "What is the employee's last name?"
+                },
+                {
+                    name: "title",
+                    type: "rawlist",
+                    message: "What is the employee's title?",
+                    choices: function () {
+                        let choiceArray = [];
+                        for (let i = 0; i < res.length; i++) {
+                            choiceArray.push(res[i].title);
+                        }
+                        return choiceArray;
+                    }
+                }
+            ])
+            .then(function (answer) {
+                console.log(answer.title)
+                let title = answer.title;
+                switch (title) {
+                    case "Accountant":
+                        title = 1;
+                        break;
+                    case "Analyst":
+                        title = 2;
+                        break;
+                    case "Legal Associate":
+                        title = 3;
+                        break;
+                    case "Partner":
+                        title = 4;
+                        break;
+                    case "Actuary":
+                        title = 5;
+                        break;
+                    case "Lawyer":
+                        title = 6;
+                        break;
+                }
+                let query = `INSERT INTO employee SET ?`;
+                // "INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)"
+                connection.query(query,
+                    {
+                        first_name: answer.first_name,
+                        last_name: answer.last_name,
+                        role_id: title
+                    },
+                    function (err, res) {
+                        if (err) throw err;
+                        console.table(res);
+                        console.log(res.insertedRows + " Rows inserted")
+                        runSearch();
+                    }
+                )
+            })
     })
 }
